08029a2f65a283d6fcd45a6df924f12dd276e8b2
pareebee <85407068+pareebee@users.noreply.github.com>
2021-06-07 06:23:08 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,49 @@
 import discord
 import os
-#import pynacl
-#import dnspython
+import random
 import server
+import aiohttp
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+names = []
+intents = discord.Intents(messages=True, guilds=True)
+
+Client = commands.Bot(command_prefix = '/', intents = discord.Intents.all())
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+@Client.event
+async def on_ready():
+    print('Bot is ready.')
+    for guild in Client.guilds:
+          for member in guild.members:
+            name = member.name 
+            print (name)
+            names.append(name)
+
+@Client.command(name="minutes")
+async def some_crazy_function_name(ctx):
+  name = random.choice(names)
+  await ctx.channel.send("The notetaker for today's meeting is "+ name + ":)")
+
+
+@Client.command (name="link") 
+async def link(ctx): 
+        embed = discord.Embed(
+            color= discord.Colour.orange() # or any color you want
+        )
+        embed.add_field(name='Click here to join the meeting' ,value='[Zoom Meeting]( https://mit.zoom.us/j/93236606772 )', inline=False)
+        await ctx.send(embed=embed)
+       
+@Client.command(pass_context=True)
+async def cat(ctx):
+    embed = discord.Embed(title="", description="")
+
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/lolcats/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed)
 
 server.server()
-bot.run(TOKEN)
+Client.run(TOKEN)
